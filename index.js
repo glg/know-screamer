ed5e2dd95659bc3faf18cee487d067b68cdc5876
Magno West <mwest@glgroup.com>
2022-01-17 15:24:35 +0000

    change file name

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
 
     // This should be a list of files you want to scream at
     const filesToCheck = [
-      "wikis.json"
+      "./scripts/wikis.json"
     ];
 
     const dirsTocheck = await getAllRelevantFiles(files, filesToCheck);
